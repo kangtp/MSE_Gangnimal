14f48824e77de33be31cc274dd3522d30cf9c502
kangtp <115209262+kangtp@users.noreply.github.com>
2024-04-18 22:16:02 +0900

    PowerGauge UI

---
+++ Gangnimal/Assets/Script/PowerGauge.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerGage : MonoBehaviour
+{
+    private float clickTime = 0;
+    private float powerValue = 0;
+    private bool isClick = false;
+    private float maxClickTime = 1f;
+    private bool timeUp = true;
+
+    public Slider powerSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            isClick = true;
+            Debug.Log("clicked");
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isClick = false;
+            Debug.Log("Release");
+        }
+
+        if (isClick)
+        {
+            if(clickTime >= maxClickTime)
+            {
+                timeUp = false;
+            }
+            else if(clickTime <= 0)
+            {
+                timeUp = true;
+            }
+            if (timeUp)
+            {
+                clickTime += Time.deltaTime;
+            }
+            else
+            {
+                clickTime -= Time.deltaTime;
+            }
+            //Debug.Log(clickTime);
+        }
+        else
+        {
+            clickTime = 0;
+        }
+
+        if(powerSlider != null)
+        {
+            powerValue = clickTime / maxClickTime;
+            powerSlider.value = powerValue;
+        }
+    }
+}
