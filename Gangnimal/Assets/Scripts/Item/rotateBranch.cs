d05b216aca070b64a343421fe11972aca741f978
kangtp <115209262+kangtp@users.noreply.github.com>
2024-04-19 12:48:12 +0900

    main organize

---
+++ Gangnimal/Assets/Scripts/Item/rotateBranch.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBranch : MonoBehaviour
+{
+
+    public int speed = 100;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
