778f860549791c15a2eef5cc5ff48ab29b4b87d5
kangtp <115209262+kangtp@users.noreply.github.com>
2024-05-09 20:36:06 +0900

    Update MainUIButton.cs

--- Gangnimal/Assets/Scripts/UI/MainUIButton.cs
+++ Gangnimal/Assets/Scripts/UI/MainUIButton.cs
@@ -8,7 +8,7 @@ public class MainUIButton : MonoBehaviour
 
     public void StartButton()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("ForestScene");
     }
 
     public void OptionButton()
