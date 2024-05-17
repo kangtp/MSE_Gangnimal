7d1c228584f6a3561de46b100a18ba1a6907192a
ykd0638 <57110958+ykd-yang@users.noreply.github.com>
2024-05-17 11:32:33 +0900

    asdsad

--- Gangnimal/Assets/Scripts/UI/MainUIButton.cs
+++ Gangnimal/Assets/Scripts/UI/MainUIButton.cs
@@ -8,7 +8,7 @@ public class MainUIButton : MonoBehaviour
 
     public void StartButton()
     {
-        SceneManager.LoadScene("ForestScene");
+        SceneManager.LoadScene("CharacterSelect");
     }
 
     public void OptionButton()
