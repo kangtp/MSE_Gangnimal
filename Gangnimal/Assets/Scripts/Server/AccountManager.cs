3bc9fbde318c5435cd7336a1d4158f0d50a64604
kangtp <115209262+kangtp@users.noreply.github.com>
2024-05-23 22:02:59 +0900

    battle record system_init

--- Gangnimal/Assets/Scripts/Server/AccountManager.cs
+++ Gangnimal/Assets/Scripts/Server/AccountManager.cs
@@ -9,6 +9,7 @@ public class AccountManager : MonoBehaviour
 {
     private string signUpUrl = "http://localhost:7755/gangnimal/signup";
     private string signInUrl = "http://localhost:7755/gangnimal/signin";
+    private string updateRecordUrl = "http://localhost:7755/gangnimal/update/record/";
 
     //for Sign In
     public TMP_InputField nickNameInput;
@@ -27,6 +28,14 @@ public class AccountManager : MonoBehaviour
     public void SignIn()
     {
         StartCoroutine(signInRequest());
+    }
+
+    public void UpdateBattleRecord()
+    {
+        updateRecordUrl += UserInfo.Instance.userName + "/win";  //later, bring the string that stored battle result.
+        Debug.Log(updateRecordUrl);
+        StartCoroutine(RecordUpdateRequest());
+        updateRecordUrl = "http://localhost:7755/gangnimal/update/record/";
     }
 
     IEnumerator signUpRequest()
@@ -51,9 +60,15 @@ public class AccountManager : MonoBehaviour
                 break;
             case UnityWebRequest.Result.Success:
                 if (www.downloadHandler.text == "true")
+                {
                     Debug.Log("Create Account Success!");
+                    //UserInfo.Instance.userName = a.nickName;
+                }
                 else
+                {
                     Debug.Log("Account creation Failed! The nickname you entered already exists.");
+                }
+                    
                 break;
         }
     }
@@ -82,14 +97,37 @@ public class AccountManager : MonoBehaviour
                 if (www.downloadHandler.text == "true")
                 {
                     Debug.Log("Success! Login...");
+                    UserInfo.Instance.userName = a.nickName;
+                    Debug.Log("Hi! "+ UserInfo.Instance.userName);
                     SceneManager.LoadScene("MainMenu");
                 }
-
                 else
                 {
                     Debug.Log("Failed! Invalid Account");
                 }
                     
+                break;
+        }
+    }
+
+    IEnumerator RecordUpdateRequest()
+    {
+
+        UnityWebRequest www = UnityWebRequest.Get(updateRecordUrl);
+        www.SetRequestHeader("Accept", "application/json");
+        yield return www.SendWebRequest();
+
+        switch (www.result)
+        {
+            case UnityWebRequest.Result.ConnectionError:
+            case UnityWebRequest.Result.DataProcessingError:
+                Debug.Log("Error: " + www.error);
+                break;
+            case UnityWebRequest.Result.ProtocolError:
+                Debug.Log("HTTP Error: " + www.error);
+                break;
+            case UnityWebRequest.Result.Success:
+                Debug.Log("Update Record...");
                 break;
         }
     }
