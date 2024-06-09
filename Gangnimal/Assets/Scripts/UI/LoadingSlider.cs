500c702cda659c0313c243e9f00ae01f8c02031c
ykd0638 <ykd0638@naver.com>
2024-06-10 05:31:50 +0900

    LoadingSystem 구현
    로딩 시스템 구현 : 스폰과정에 있어서의 기다림 동안 로딩 ui 보여주고 일정시간 지나면 꺼짐 5

---
+++ Gangnimal/Assets/Scripts/UI/LoadingSlider.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSlider : MonoBehaviour
+{
+    public Slider slider; // 로딩 슬라이드바
+    public float loadTime = 5f; // 대기시간
+    public Button hostButton;
+    private float timer = 0f;
+    bool clicked=false;//중복클릭 방지용으로 false
+
+    void Start()
+    {
+        // 초기화
+        slider.value = 0f;
+        
+        //테스트용
+        hostButton.onClick.Invoke();
+    }
+
+    void Update()
+    {
+        
+        timer += Time.deltaTime;
+
+        
+        slider.value = Mathf.Clamp01(timer / loadTime);
+
+        if(timer>0.1f &&!clicked) // 딱 1번만 
+        {
+            hostButton.onClick.Invoke();
+            clicked=true;
+        }
+        if (slider.value >= 1f)
+        {
+
+            OnLoadingComplete();
+            
+        }
+    }
+
+    void OnLoadingComplete()
+    {
+        // 캔바스 끄기
+        gameObject.SetActive(false);
+    }
+}
